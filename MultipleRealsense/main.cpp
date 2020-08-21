fae1949e3dd48f31c902a7907a6c9464dd2078f0
mikihiroikura <miki.ut.sikepuri@gmail.com>
2020-08-22 00:07:03 +0900

    複数のRealsenseの出力

--- MultipleRealsense/main.cpp
+++ MultipleRealsense/main.cpp
@@ -7,10 +7,20 @@
 
 using namespace std;
 
+struct Realsense
+{
+	cv::Mat color;
+	cv::Mat depth;
+	rs2::frame colorframe;
+	rs2::frame depthframe;
+	rs2::pipeline pipe;
+	rs2::config config;
+	rs2::frameset frames;
+};
+
 int main() {
 	rs2::context context;
-	rs2::config config;
-	vector<rs2::pipeline> pipes;
+	vector<Realsense> rs_devices;
 
 	try
 	{
@@ -18,45 +28,47 @@ int main() {
 		const rs2::device_list device_list = context.query_devices();
 		for (rs2::device device : device_list)
 		{
-			rs2::pipeline pipe;
+			Realsense rs;
 			cout << device.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER) << endl;
-			config.enable_device(device.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER));
-			config.enable_stream(RS2_STREAM_COLOR, 1920, 1080, RS2_FORMAT_BGR8, 30);
-			config.enable_stream(RS2_STREAM_DEPTH, 1280, 720, RS2_FORMAT_Z16, 30);
+			rs.config.enable_device(device.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER));
+			rs.config.enable_stream(RS2_STREAM_COLOR, 1920, 1080, RS2_FORMAT_BGR8, 30);
+			rs.config.enable_stream(RS2_STREAM_DEPTH, 1280, 720, RS2_FORMAT_Z16, 30);
 
-			pipe.start(config);
+			rs.pipe.start(rs.config);
 
-			pipes.push_back(pipe);
+			rs_devices.push_back(rs);
 		}
 		rs2::colorizer color_map;
 
-		rs2::frameset frames;
 		for (int i = 0; i < 30; i++)
 		{
-			for (rs2::pipeline& pipe : pipes)
+			for (Realsense rs: rs_devices)
 			{
 				//Wait for all configured streams to produce a frame
-				frames = pipe.wait_for_frames();
+				rs.frames = rs.pipe.wait_for_frames();
 			}
 		}
 
 		while (true)
 		{
-			for (rs2::pipeline& pipe : pipes)
+			for (int i=0; i<rs_devices.size(); i++)
 			{
-				frames = pipe.wait_for_frames();
-				rs2::frame colorframe = frames.get_color_frame();
-				rs2::frame depthframe = (frames.get_depth_frame()).apply_filter(color_map);
+				rs_devices[i].frames = rs_devices[i].pipe.wait_for_frames();
+				rs_devices[i].colorframe = rs_devices[i].frames.get_color_frame();
+				rs_devices[i].depthframe = (rs_devices[i].frames.get_depth_frame()).apply_filter(color_map);
 
 				// Creating OpenCV Matrix from a color image
-				cv::Mat color(cv::Size(1920, 1080), CV_8UC3, (void*)colorframe.get_data(), cv::Mat::AUTO_STEP);
-				cv::Mat depth(cv::Size(1280, 720), CV_8UC3, (void*)depthframe.get_data(), cv::Mat::AUTO_STEP);
-
+				rs_devices[i].color = cv::Mat(cv::Size(1920, 1080), CV_8UC3, (void*)rs_devices[i].colorframe.get_data(), cv::Mat::AUTO_STEP);
+				rs_devices[i].depth = cv::Mat(cv::Size(1280, 720), CV_8UC3, (void*)rs_devices[i].depthframe.get_data(), cv::Mat::AUTO_STEP);
+				
 				// Display in a GUI
-				cv::namedWindow("Display Color Image", cv::WINDOW_AUTOSIZE);
-				cv::namedWindow("Display Depth Image", cv::WINDOW_AUTOSIZE);
-				cv::imshow("Display Color Image", color);
-				cv::imshow("Display Depth Image", depth);
+				string colorwinname = "Display Color Image " + to_string(i);
+				string depthwinname = "Display Depth Image " + to_string(i);
+
+				cv::namedWindow(colorwinname, cv::WINDOW_AUTOSIZE);
+				cv::namedWindow(depthwinname, cv::WINDOW_AUTOSIZE);
+				cv::imshow(colorwinname, rs_devices[i].color);
+				cv::imshow(depthwinname, rs_devices[i].depth);
 
 				
 			}
